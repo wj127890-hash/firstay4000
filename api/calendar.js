79e34ea0b8c031cc1118a23ea71abd9c99c9b573
wj127890-hash <wj127890@gmail.com>
2026-04-11 00:48:52 +0900

    Update calendar.js

--- api/calendar.js
+++ api/calendar.js
@@ -1,1 +1,12 @@
 
+export default async function handler(req, res) {
+  const AIRBNB_URL = "https://www.airbnb.co.kr/calendar/ical/1449092803394676993.ics?t=ae54c99940fb41e998f0b0b30f34e0ea";
+  try {
+    const response = await fetch(AIRBNB_URL);
+    const data = await response.text();
+    res.setHeader('Content-Type', 'text/calendar');
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send("Error");
+  }
+}
